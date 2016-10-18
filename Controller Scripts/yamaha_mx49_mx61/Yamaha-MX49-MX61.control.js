bd003427dec7aa04c8b6948b219dfa8fb47c0341
ngradwohl <guru@local-guru.net>
2016-10-18 20:53:39 +0200

    print macro names to lcd display

--- Controller Scripts/yamaha_mx49_mx61/Yamaha-MX49-MX61.control.js
+++ Controller Scripts/yamaha_mx49_mx61/Yamaha-MX49-MX61.control.js
@@ -20,26 +20,127 @@ function init() {
    host.getMidiInPort(0).setMidiCallback(onMidi0);
    host.getMidiInPort(1).setMidiCallback(onMidi1);
 
+   host.getMidiInPort(4).setMidiCallback(onMidi5);
+   host.getMidiInPort(4).setSysexCallback(onSysex5);
+
    generic = host.getMidiInPort(0).createNoteInput("Yamaha MX49/MX61");
+   out1 = host.getMidiOutPort(0)
+
+   out2 = host.getMidiOutPort(1)
+   out3 = host.getMidiOutPort(2)
+   out4 = host.getMidiOutPort(3)
+   out5 = host.getMidiOutPort(4)
+
    generic.setShouldConsumeEvents(false);
+
+   out5.sendSysex("F0 43 10 7F 17 01 20 00 00 F7")
+
+    sendTemplateName( "Bitwig");
+    sendName(8, "Vol")
+    sendName(9, "Pan")
+    sendName(10, "Send 1")
+    sendName(11, "Send 2")
+
+    for( var i= 0; i<8; i++) {
+        sendValue( i, 0, macromapped[i] );
+    }
 
    transport = host.createTransportSection();
    application = host.createApplication();
 
    cursorTrack = host.createCursorTrack(0, 0);
-   primaryDevice = cursorTrack.getPrimaryDevice();
+   
+   //primaryDevice = cursorTrack.getPrimaryDevice();
+   primaryDevice = cursorTrack.createCursorDevice()
 
+   primaryDevice.addNameObserver( 15, "Device", function( name ) {
+        sendTemplateName( name );        
+        out5.sendSysex("F0 43 10 7F 17 01 20 00 00 F7")
+
+   })
+
+   for( var i=0; i<8; i++) {
+       primaryDevice.getMacro( i ).addLabelObserver( 24, "Maxro "+i, getMacroNameObserver(i))                
+       primaryDevice.getMacro( i ).getAmount().addValueObserver(100,getValueObserver( i ));
+       primaryDevice.getMacro( i ).getModulationSource().addIsMappedObserver( getMacroIsMappedObserver( i ));
+   }
+}
+
+macromapped = [ true, true, true, true, true, true, true ,true ];
+macrovalue = [0,0,0,0,0,0,0,0]
+function getMacroIsMappedObserver( i ) {
+    return function( mapped ) {
+        macromapped[i] = mapped;
+        sendValue(i,macrovalue[i],mapped);
+    }
+}
+
+function getMacroNameObserver( i ) {
+    return function( macroname ) {
+        sendName( i, macroname );
+    }
+}
+
+function getValueObserver( i ) {
+    return function( value ) {
+        macrovalue[i] = value
+        sendValue(i, value, macromapped[i] );  
+    }
 }
 
 function onMidi0(status, data1, data2) {
     //printMidi(status, data1, data2);
 }
 
+function sendTemplateName( name ) {
+    for ( var i=0; i<15; i++) {
+        var c = i<name.length ? name.charCodeAt(i) : 0x20;
+        out5.sendSysex("F0 43 10 7F 17 01 00 "+hexpad(i)+" "+hexpad(c)+" F7")
+    }
+}
+
+function sendName( param, name ) {
+    var str = "F0 43 10 7F 17 01 "+hexpad(48+param)+" 09 ";
+
+    for( var i=0; i<24; i++) {
+        var cc = i < name.length ? name.charCodeAt(i) : 0x20;
+        str += hexpad(cc) + " "
+    }
+    str += "F7"
+    out5.sendSysex(str)
+}
+
+function sendValue( param, value, active ) {
+    var str = "F0 43 10 7F 17 01 "+hexpad(0x30+param)+" 21 "
+    var valstr = active ? value.toString(10) : "-";
+    for ( var i=0; i< 8; i++) {
+        var c = i < valstr.length ? valstr.charCodeAt(i) : 0x20;
+        str += hexpad(c)+" ";
+    }
+    str += (active ? "00 " : "01 ") + "00 F7"
+    out5.sendSysex(str);
+}
+
+function hexpad( i ) {
+    var t = "00"+i.toString(16);
+    return t.substr(t.length-2);
+}
+
+function onMidi5(status, data1, data2) {
+  //  printMidi(status, data1, data2);
+}
+
+
+function onSysex5(data) {
+//    printMidi(data)
+}
+
 function onMidi1(status, data1, data2) {
     printMidi(status, data1, data2);
     if ( isPitchBend( status )) {
-        println( "Control: " +  MIDIChannel( status ))
         var idx  = MIDIChannel( status );
+        sendValue(idx, Math.round(100*data2/127), idx >= 8 || macromapped[idx]);
+
         if ( idx < 8 ) {
             primaryDevice.getMacro( idx ).getAmount().set(data2, 128);
         } else if ( idx == 8 ) {
